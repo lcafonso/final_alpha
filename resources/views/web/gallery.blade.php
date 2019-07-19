7c2bfd8495a19788eecb80fe8d7128112dc64831
lcafonso <lcafonso@gmail.com>
2019-07-19 14:21:56 +0100

    novo tema

--- resources/views/web/gallery.blade.php
+++ resources/views/web/gallery.blade.php
@@ -4,10 +4,15 @@
 @section('head')
     {{-- Another scripts and styles in head--}}
 
+    <script type="text/javascript" src="{{asset('vendor/unitegallery/dist/js/unitegallery.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('vendor/unitegallery/dist/css/unite-gallery.css')}}">
+
+    <script type="text/javascript" src="{{asset('vendor/unitegallery/dist/themes/tiles/ug-theme-tiles.js')}}"></script>
     <style>
         body{
             background: -webkit-linear-gradient(left, #33775f, #fff);
         }
+
 
 
     </style>
@@ -19,13 +24,27 @@
 
 @section('content')
 
-    <!-- Page Content -->
-    <div class="container">
 
-        Galleria
+
+    <!-- Page Content -->
+    <div id="gallery" style="display:none;">
+
+        @foreach ($posts as $post)
+        <img alt="Preview Image 1"
+             src="{{ asset($post->file)  }}"
+             data-image="{{ asset($post->file) }}"
+             data-description="Preview Image 1 Description">
+        @endforeach
+        <!-- Pagination -->
+        <div>
+            {{ $posts->render() }}
+        </div>
 
     </div>
-    <!-- /.container -->
+
+
+
+
 
 @endsection
 
@@ -33,6 +52,16 @@
 @section('footer')
 
     {{-- Another scripts and styles in footer --}}
+    <script type="text/javascript">
 
+        jQuery(document).ready(function(){
+
+            jQuery("#gallery").unitegallery({
+                gallery_theme: "tiles",
+                tiles_type: "justified"
+            });
+        });
+
+    </script>
 
 @endsection
